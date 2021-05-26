f4fa2b9cbece6a827d507f930e680d0031cde0b8
diegogomezlopez <diegogomezlopez91@gmail.com>
2021-05-26 22:26:30 +0200

    ADD PriceServiceException

---
+++ src/main/java/com/zara/zaratest/exception/PriceServiceException.java
@@ -0,0 +1,8 @@
+package com.zara.zaratest.exception;
+
+public class PriceServiceException extends RuntimeException {
+
+    public PriceServiceException(final String message) {
+        super(message);
+    }
+}
