eab90a9e58861b32f06af1c9242b46757991d9e6
diegogomezlopez <diegogomezlopez91@gmail.com>
2021-05-26 22:29:25 +0200

    UPDATE PriceServiceImpl

--- src/main/java/com/zara/zaratest/service/impl/PriceServiceImpl.java
+++ src/main/java/com/zara/zaratest/service/impl/PriceServiceImpl.java
@@ -1,16 +1,25 @@
 package com.zara.zaratest.service.impl;
 
-import com.zara.zaratest.dto.PriceResponse;
+import com.zara.zaratest.domain.Price;
+import com.zara.zaratest.dto.PriceSearchRequest;
+import com.zara.zaratest.dto.PriceSearchResponse;
+import com.zara.zaratest.repository.H2PriceRepository;
+import com.zara.zaratest.repository.PriceRepository;
 import com.zara.zaratest.service.PriceService;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 
 @Service
 public class PriceServiceImpl implements PriceService {
 
+    private final PriceRepository repository;
+
+    public PriceServiceImpl(final H2PriceRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
-    public PriceResponse getPrice(final LocalDateTime date, final Integer product_id, final Integer brand_id) {
+    public PriceSearchResponse getPrice(final PriceSearchRequest request) {
+        Price price = repository.search(request.getDate(), request.getProductId(), request.getBrandId());
         return null;
     }
 }
